ee0cd1999512f7e91779d9de59fb29b7d171b552
rendixk <ferendfio@gmail.com>
2025-11-06 16:02:46 +0700

    initial

--- server/models/index.js
+++ server/models/index.js
@@ -10,9 +10,6 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-sequelize.authenticate()
-  .then(() => console.log("Databse connected"))
-  .catch(err => console.error("Database connection error", err))
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
